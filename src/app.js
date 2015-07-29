1dc5a75ca81b9041cea63ffda0a3bc0cd6699d63
crccheck <c@crccheck.com>
2015-07-28 21:54:17 -0500

    hi, I am a react app

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
 console.log('hello')
 const css = require('../style/app.scss');
+
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <textarea/>
+      </div>
+    )
+  }
+}
+
+
+// main
+React.render(<App/>, document.getElementById('app'));
