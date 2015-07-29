168d8574174b786ec484fe96f4f54210de59f259
crccheck <c@crccheck.com>
2015-07-28 22:33:57 -0500

    nm, use react 0.14 beta to get ref dom goodness

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ console.log('hello')
 const css = require('../style/app.scss');
 
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  textInput(e) {
+    console.log(this.refs.input, e)
+  }
+
   render() {
     return (
       <div>
-        <textarea/>
+        <textarea ref="input" onKeyUp={this.textInput.bind(this)}/>
       </div>
     )
   }
@@ -16,4 +20,4 @@ class App extends React.Component {
 
 
 // main
-React.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
