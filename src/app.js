0adcd6773fd23f06d27fb09f0309e93313310cf8
crccheck <c@crccheck.com>
2015-07-28 22:47:10 -0500

    add unique number of characters entered

--- src/app.js
+++ src/app.js
@@ -1,18 +1,35 @@
 console.log('hello')
 const css = require('../style/app.scss');
 
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      cardinality: 0
+    };
+  }
+
+  cardinality(str) {
+    return _.uniq(str.split('')).length;
+  }
+
   textInput(e) {
-    console.log(this.refs.input, e)
+    var input = this.refs.input.value.replace(/\s+/g, '');
+    var cardinality = this.cardinality(input);
+    this.setState({input, cardinality});
   }
 
   render() {
     return (
       <div>
         <textarea ref="input" onKeyUp={this.textInput.bind(this)}/>
+        Unique: <span>{this.state.cardinality}</span>
       </div>
     )
   }
